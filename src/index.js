623921cb2fe92125f8895a450fe50bd55a1de3b2
marcos-dev-web <marcos.dev.web.frontend@gmail.com>
2021-10-31 16:41:29 -0300

    add more limit for request

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const { productRouter } = require('./routes/product');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({
+  limit: '50mb'
+}));
 app.use(helmet());
 app.use(
   cors({
